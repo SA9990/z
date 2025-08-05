69dab8d6dc0804936fa1f1f5e7d0045f52895426
Rocky7842 <eric.rocky7842@gmail.com>
2025-08-06 00:15:33 +0800

    input: misc: qpnp-power-on: Fix misleading indentation
    Change-Id: I5596558b32451aedf938e2b56ce3633fb985dce9

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -1702,12 +1702,12 @@ qpnp_pon_request_irqs(struct qpnp_pon *pon, struct qpnp_pon_config *cfg)
 
 	/* Mark the interrupts wakeable if they support linux-key */
 	if (cfg->key_code) {
-	printk("[d_keypad] key_code=%d irq=%d \n",cfg->key_code, cfg->state_irq);
+		printk("[d_keypad] key_code=%d irq=%d \n",cfg->key_code, cfg->state_irq);
 
-	if(gresin_irq == cfg->state_irq)
-		asus_enable_resin_irq_wake(1);
-	else
-		enable_irq_wake(cfg->state_irq);
+		if(gresin_irq == cfg->state_irq)
+			asus_enable_resin_irq_wake(1);
+		else
+			enable_irq_wake(cfg->state_irq);
 
 		/* Special handling for RESIN due to a hardware bug */
 		if (cfg->pon_type == PON_RESIN && cfg->support_reset)
